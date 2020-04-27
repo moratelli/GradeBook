f405147138ffb54baae3afd884cbc9b7d014e9d0
Pedro Moratelli <pedromoratelli@gmail.com>
2020-04-27 17:26:21 -0300

    Encapsulate ShowStats in the Book class

--- GradeBook/Book.cs
+++ GradeBook/Book.cs
@@ -5,16 +5,41 @@ namespace GradeBook
 {
     class Book
     {
-        public Book()
-        {
+        public Book(string name)
+        {   
             grades = new List<double>();
+            this.name = name;
         }
 
         public void AddGrade(double grade)
-        {
-            grades.Add(grade);
+        {   
+            if(grade > 0 && grade < 100)
+            {
+                grades.Add(grade);
+            }
+            
         }
 
-        List<double> grades;
+        public void ShowStats()
+        {
+            var result = 0.0;
+            var highGrade = double.MinValue;
+            var lowGrade = double.MaxValue;
+
+            foreach (double number in grades)
+            {
+                highGrade = Math.Max(number, highGrade);
+                lowGrade = Math.Min(number, lowGrade);
+                result += number;
+            }
+            result /= grades.Count;
+
+            Console.WriteLine($"The lowest grade is {lowGrade}");
+            Console.WriteLine($"The highest grade is {highGrade}");
+            Console.WriteLine($"The average grade is {result:N1}");
+        }
+
+        private List<double> grades;
+        private string name;
     }
 }
