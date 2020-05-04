ad1055af602362947aba82fa17588d329a0ea15d
Pedro Moratelli <pedromoratelli@gmail.com>
2020-05-04 14:48:07 -0300

    Create Tests

--- test/GradeBook.Tests/BookTests.cs
+++ test/GradeBook.Tests/BookTests.cs
@@ -6,7 +6,7 @@ namespace GradeBook.Tests
     public class BookTests
     {
         [Fact]
-        public void Test1() {
+        public void BookCalculatesGrades() {
             
             //  arrange
             var book = new Book("");
